93960a6754cddd8d678d6f1e226cb4c835724aab
Tayef_Shah <tayefshah@gmail.com>
2019-06-02 02:01:48 -0400

    add location mapping script

--- location.js
+++ location.js
@@ -1,2 +1,4 @@
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnjUvZEKiJGuEyAb_Kow4i3TSRicYVu0U&callback=initMap"
+const GOOGLE_API_KEY = "AIzaSyAnjUvZEKiJGuEyAb_Kow4i3TSRicYVu0U";
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap",
   type="text/javascript"></script>
