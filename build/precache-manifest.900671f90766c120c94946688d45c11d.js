a64f01b2f1e01a788bc81d658540520db90432fa
strawberrryyy <51021835+strawberrryyy@users.noreply.github.com>
2019-07-04 12:06:45 -0400

    Add files via upload

---
+++ build/precache-manifest.900671f90766c120c94946688d45c11d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a50e2d9761e9af7c1f930fb208a859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ca29d6058f80de91ab",
+    "url": "/static/css/main.9953e768.chunk.css"
+  },
+  {
+    "revision": "8ab9b009d9629b2fc47e",
+    "url": "/static/js/2.84e536ea.chunk.js"
+  },
+  {
+    "revision": "33ca29d6058f80de91ab",
+    "url": "/static/js/main.600f3c01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eddf8f48a1b60a4d2f5489f922395095",
+    "url": "/static/media/carbonfootprintvisuallightgreen.eddf8f48.svg"
+  },
+  {
+    "revision": "974191ca8f7a7e66f28d8cb62006c698",
+    "url": "/static/media/forestimage.974191ca.png"
+  },
+  {
+    "revision": "27434945a5a6f8ab9a7a00ab57b611f8",
+    "url": "/static/media/ottercartheaderlogo.27434945.png"
+  },
+  {
+    "revision": "2d55e029b700485b0c076b71d2fae9e1",
+    "url": "/static/media/shoppingcart.2d55e029.png"
+  }
+]);
